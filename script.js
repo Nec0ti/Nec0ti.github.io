1c156755c4b17c0fef2da416778a0afd8e21e958
Necoti <halilnecatig2@gmail.com>
2024-07-03 12:20:31 +0300

    changing style

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function ReadMore() {
-    //open article1.html
-    window.location.href = "articles/article1.html";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Kişisel portfolyo sitesi yüklendi.');
+
+    const links = document.querySelectorAll('.sidebar ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
